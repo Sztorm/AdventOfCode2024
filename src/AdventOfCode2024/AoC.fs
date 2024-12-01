cd91f7154a2c6278d04b771d5d1a135bb08360ff
Sztorm <marcinwicher99@gmail.com>
2024-12-01 19:12:46 +0100

    AoC2024 setup.

---
+++ src/AdventOfCode2024/AoC.fs
@@ -0,0 +1,7 @@
+﻿module AoC
+
+open System.IO
+
+let private basePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+let readInput (path: string): string array = File.ReadAllLines(Path.Combine(basePath, path))
