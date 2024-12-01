61c9ddd86b15ed47e7bf8f39874780e701fbb727
Sztorm <marcinwicher99@gmail.com>
2024-12-01 21:18:57 +0100

    Added solution of the second part of the day 1.

--- src/AdventOfCode2024/Day01/Day01.fs
+++ src/AdventOfCode2024/Day01/Day01.fs
@@ -16,4 +16,25 @@ let part1 (input: string array): int64 =
        )
     |> Array.sum
 
-let part2 (input: string array): int64 = 0
+let part2 (input: string array): int64 =
+    let inputPairs =
+        input 
+        |> Array.map (fun s -> 
+            let stringPair = s.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            (int64 stringPair[0], int64 stringPair[1])
+        )
+    let occurenceCountsByNumber = 
+        inputPairs
+        |> Array.fold (fun acc (_, right) -> 
+            let occurenceCount = 
+                match acc |> Map.tryFind right with
+                | Some count -> count
+                | None -> 0
+            acc |> Map.add right (occurenceCount + 1)
+        ) Map.empty
+    inputPairs
+    |> Array.sumBy (fun (left, _) ->
+        match occurenceCountsByNumber |> Map.tryFind left with
+        | Some count -> int64 count * left
+        | None -> 0
+    )
