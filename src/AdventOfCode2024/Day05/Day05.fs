21d00c696b9e8ce4e40e822d040f8b3be582cd22
Sztorm <marcinwicher99@gmail.com>
2024-12-07 03:38:29 +0100

    Added solution of the first part of the day 5.

---
+++ src/AdventOfCode2024/Day05/Day05.fs
@@ -0,0 +1,33 @@
+﻿module Day05
+
+let part1 (input: string array): int64 =
+    let pageOrderingRules =
+        input
+        |> Array.takeWhile (fun s -> s.Length <> 0)
+        |> Array.map (fun s ->
+            let xy = s.Split('|')
+            let x = int xy[0]
+            let y = int xy[1]
+
+            (x, y)
+        )
+        |> Set
+
+    let updates =
+        input
+        |> Array.skip (pageOrderingRules.Count + 1)
+        |> Array.map (fun s -> s.Split(',') |> Array.map int)
+
+    let sortedUpdates =
+        updates
+        |> Array.map (fun update ->
+            update
+            |> Array.sortWith (fun a b ->
+                if (pageOrderingRules |> Set.contains (a, b)) then -1 else 1
+            )
+        )
+        
+    (updates, sortedUpdates)
+    ||> Array.fold2 (fun acc l r -> acc + int64 (if (l = r) then l[Array.length l / 2] else 0)) 0L
+
+let part2 (input: string array): int64 = 0
--- src/AdventOfCode2024/Program.fs
+++ src/AdventOfCode2024/Program.fs
@@ -2,10 +2,10 @@
 
 [<EntryPoint>]
 let main (args: string array): int =
-    let testInput = AoC.readInput "Day04/testInput.txt"
-    let input = AoC.readInput "Day04/input.txt"
-    printfn "part 1 test: %d" (Day04.part1 testInput)
-    printfn "part 1:      %d" (Day04.part1 input)
-    printfn "part 2 test: %d" (Day04.part2 testInput)
-    printfn "part 2:      %d" (Day04.part2 input)
+    let testInput = AoC.readInput "Day05/testInput.txt"
+    let input = AoC.readInput "Day05/input.txt"
+    printfn "part 1 test: %d" (Day05.part1 testInput)
+    printfn "part 1:      %d" (Day05.part1 input)
+    printfn "part 2 test: %d" (Day05.part2 testInput)
+    printfn "part 2:      %d" (Day05.part2 input)
     0
