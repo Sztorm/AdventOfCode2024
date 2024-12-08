b4b7d7e5c8042cdaa1979e0fefe3c550c3604dbe
Sztorm <marcinwicher99@gmail.com>
2024-12-08 13:36:14 +0100

    Added solution of the second part of the day 8.

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ Solutions for the 2024 edition of **[Advent of Code](https://adventofcode.com/20
 - [Day 5](src/AdventOfCode2024/Day05/Day05.fs) ⭐⭐
 - [Day 6](src/AdventOfCode2024/Day06/Day06.fs) ⭐⭐
 - [Day 7](src/AdventOfCode2024/Day07/Day07.fs) ⭐⭐
-- [Day 8](src/AdventOfCode2024/Day08/Day08.fs) ⭐
+- [Day 8](src/AdventOfCode2024/Day08/Day08.fs) ⭐⭐
 
-Collected stars: **15**
+Collected stars: **16**
 
 ## License
 [MIT](https://github.com/Sztorm/AdventOfCode2024/blob/master/LICENSE.txt)
--- src/AdventOfCode2024/Day08/Day08.fs
+++ src/AdventOfCode2024/Day08/Day08.fs
@@ -2,24 +2,25 @@
 
 open ForgivingArray2D
 
-let part1 (input: string array): int64 =
-    let positionsByAntenna =
-        let rec loop r c result =
-            if r < Array.length input then
-                if c < String.length input[r] then
-                    match input[r][c] with
-                    | '.' -> loop r (c + 1) result
-                    | antenna ->
-                        match result |> Map.tryFind antenna with
-                        | Some positions ->
-                            loop r (c + 1) (result |> Map.add antenna ((r, c)::positions))
-                        | None -> loop r (c + 1) (result |> Map.add antenna [(r, c)])
-                else
-                    loop (r + 1) 0 result
+let positionsByAntenna (input: string array): Map<char, (int * int) list> =
+    let rec loop r c result =
+        if r < Array.length input then
+            if c < String.length input[r] then
+                match input[r][c] with
+                | '.' -> loop r (c + 1) result
+                | antenna ->
+                    match result |> Map.tryFind antenna with
+                    | Some positions ->
+                        loop r (c + 1) (result |> Map.add antenna ((r, c)::positions))
+                    | None -> loop r (c + 1) (result |> Map.add antenna [(r, c)])
             else
-                result
-        loop 0 0 Map.empty
+                loop (r + 1) 0 result
+        else
+            result
+    loop 0 0 Map.empty
 
+let part1 (input: string array): int64 =
+    let positionsByAntenna = positionsByAntenna input
     let antinodeMap = ForgivingArray2D(Array.length input, String.length input[0], '.')
 
     positionsByAntenna
@@ -31,8 +32,11 @@ let part1 (input: string array): int64 =
             | (r1, c1)::tail ->
                 tail
                 |> List.iter (fun (r2, c2) -> (
-                    antinodeMap[2 * r1 - r2, 2 * c1 - c2] <- '#'
-                    antinodeMap[2 * r2 - r1, 2 * c2 - c1] <- '#'
+                    let (dr1, dc1) = (r1 - r2, c1 - c2)
+                    let (dr2, dc2) = (r2 - r1, c2 - c1)
+
+                    antinodeMap[r1 + dr1, c1 + dc1] <- '#'
+                    antinodeMap[r2 + dr2, c2 + dc2] <- '#'
                 ))
                 loop tail
         loop positions
@@ -41,4 +45,38 @@ let part1 (input: string array): int64 =
     antinodeMap
     |> ForgivingArray2D.sumBy (fun p -> if p = '#' then 1 else 0)
 
-let part2 (input: string array): int64 = 0
+let part2 (input: string array): int64 =
+    let positionsByAntenna = positionsByAntenna input
+    let antinodeMap = ForgivingArray2D(Array.length input, String.length input[0], '.')
+
+    positionsByAntenna
+    |> Map.values
+    |> Seq.iter (fun positions ->
+        let rec loop remainingPositions =
+            match remainingPositions with
+            | [] -> ()
+            | (r1, c1)::tail ->
+                antinodeMap[r1, c1] <- '#'
+                tail
+                |> List.iter (fun (r2, c2) -> (
+                    let (dr1, dc1) = (r1 - r2, c1 - c2)
+                    let (dr2, dc2) = (r2 - r1, c2 - c1)
+
+                    let rec loop2 r c rStep cStep =
+                        let nextR = (r + rStep)
+                        let nextC = (c + cStep)
+                        if nextR >= 0 && nextR < antinodeMap.Rows &&
+                            nextC >= 0 && nextC < antinodeMap.Columns then
+                            antinodeMap[nextR, nextC] <- '#'
+                            loop2 nextR nextC rStep cStep
+                        else ()
+
+                    loop2 r1 c1 dr1 dc1
+                    loop2 r2 c2 dr2 dc2
+                ))
+                loop tail
+        loop positions
+    )
+
+    antinodeMap
+    |> ForgivingArray2D.sumBy (fun p -> if p = '#' then 1 else 0)
