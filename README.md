01767aa6436328337c33c61233098f77fc18d298
Sztorm <marcinwicher99@gmail.com>
2024-12-10 19:31:02 +0100

    Added solution of the second part of the day 10.

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ Solutions for the 2024 edition of **[Advent of Code](https://adventofcode.com/20
 - [Day 07](src/AdventOfCode2024/Day07/Day07.fs) ⭐⭐
 - [Day 08](src/AdventOfCode2024/Day08/Day08.fs) ⭐⭐
 - [Day 09](src/AdventOfCode2024/Day09/Day09.fs) ⭐⭐
-- [Day 10](src/AdventOfCode2024/Day10/Day10.fs) ⭐
+- [Day 10](src/AdventOfCode2024/Day10/Day10.fs) ⭐⭐
 
-Collected stars: **19**
+Collected stars: **20**
 
 ## License
 [MIT](https://github.com/Sztorm/AdventOfCode2024/blob/master/LICENSE.txt)
--- src/AdventOfCode2024/Day10/Day10.fs
+++ src/AdventOfCode2024/Day10/Day10.fs
@@ -1,26 +1,27 @@
 ﻿module Day10
 
-type Direction =
-    | Up
-    | Left
-    | Down
-    | Right
+open ForgivingArray2D
+
+type Direction = Up | Left | Down | Right
+
+let findTrailheadPositions (map: ForgivingArray2D<int>) =
+    let rec loop r c positions =
+        if r < map.Rows then
+            if c < map.Columns then 
+                match map[r, c] with
+                | 0 -> loop r (c + 1) (positions @ [(r, c)])
+                | _ -> loop r (c + 1) positions
+            else
+                loop (r + 1) 0 positions
+        else positions
+    loop 0 0 []
 
 let part1 (input: string array): int64 =
     let map = ForgivingArray2D.init input.Length input[0].Length -1 (fun r c ->
         int (input[r][c]) - int '0'
     )
-    let trailheadPositions =
-        let rec loop r c positions =
-            if r < map.Rows then
-                if c < map.Columns then 
-                    match map[r, c] with
-                    | 0 -> loop r (c + 1) (positions @ [(r, c)])
-                    | _ -> loop r (c + 1) positions
-                else
-                    loop (r + 1) 0 positions
-            else positions
-        loop 0 0 []
+    let trailheadPositions = findTrailheadPositions map
+
     let trailheadScore position =
         let rec traverse r c direction peaks =
             let current = map[r, c]
@@ -77,4 +78,57 @@ let part1 (input: string array): int64 =
     trailheadPositions
     |> List.sumBy (fun position -> trailheadScore position)
 
-let part2 (input: string array): int64 = 0
+let part2 (input: string array): int64 =
+    let map = ForgivingArray2D.init input.Length input[0].Length -1 (fun r c ->
+        int (input[r][c]) - int '0'
+    )
+    let trailheadPositions = findTrailheadPositions map
+    let trailheadRating position =
+        let (thR, thC) = position
+        let rec traverse r c direction (result: int) =
+            let current = map[r, c]
+            match direction with
+            | Up ->
+                match map[r - 1, c] with
+                | p when p = current + 1 && p = 9 ->
+                    result + 1
+                | p when p = current + 1 ->               
+                    (traverse (r - 1) c Up result) +
+                    (traverse (r - 1) c Left result) +
+                    (traverse (r - 1) c Right result)
+                | _ -> result
+            | Left ->
+                match map[r, c - 1] with
+                | p when p = current + 1 && p = 9 ->
+                    result + 1
+                | p when p = current + 1 ->       
+                    (traverse r (c - 1) Up result) +
+                    (traverse r (c - 1) Left result) +
+                    (traverse r (c - 1) Down result)
+                | _ -> result  
+            | Down ->
+                match map[r + 1, c] with
+                | p when p = current + 1 && p = 9 ->
+                    result + 1
+                | p when p = current + 1 -> 
+                    (traverse (r + 1) c Left result) +
+                    (traverse (r + 1) c Down result) +
+                    (traverse (r + 1) c Right result)
+                | _ -> result
+                
+            | Right ->
+                match map[r, c + 1] with
+                | p when p = current + 1 && p = 9 ->
+                    result + 1
+                | p when p = current + 1 -> 
+                    (traverse r (c + 1) Up result) +
+                    (traverse r (c + 1) Down result) +
+                    (traverse r (c + 1) Right result)
+                | _ -> result
+        (traverse thR thC Up 0) +
+        (traverse thR thC Left 0) +
+        (traverse thR thC Down 0) +
+        (traverse thR thC Right 0)
+    
+    trailheadPositions
+    |> List.sumBy (fun position -> trailheadRating position)
